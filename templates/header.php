d0ec006784d42128ca68ea309e7a0568f1402610
Alexander <alexanderm781@gmail.com>
2013-03-15 14:44:52 -0700

    tracking code/favicon changes

--- templates/header.php
+++ templates/header.php
@@ -27,5 +27,21 @@
   <!-- Initialize JS Plugins -->
   <script src="/javascripts/app.js"></script>
   <script src="/javascripts/modernizr.foundation.js"></script>
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-17509155-7']);
+    _gaq.push(['_setSiteSpeedSampleRate', 10]);
+  _gaq.push(['_trackPageview']);
+
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<link rel="shortcut icon" href="http://dev.craftstats.com/images/favicon.ico" type="image/x-icon" />
   <?php echo $this->headscripts; ?>
 </head>
