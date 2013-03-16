4a2ccb9c8a12c73a380828a055790f2dd4648704
Alexander <alexanderm781@gmail.com>
2013-03-16 12:47:41 -0700

    robert sucks shit at coding

--- public_html/index.php
+++ public_html/index.php
@@ -35,8 +35,8 @@ FROM servers WHERE sponsorTime > UNIX_TIMESTAMP() AND blacklisted != 1 AND spons
 				<div class="twelve columns box prempromo">
 					<h5 class="subheader"><a href="/server/<?php echo $sp['ip']; ?>"><?php echo $sp['ip']; ?></a></h5>
 					<span class="subtitle"><?php echo $sp['cp'].'/'.$sp['mp'].' Players - '.$sp['uptimeavg'].'% Uptime'; ?></span><br/>
-					<?php if($sp['version'] != ''){ ?><a class="button tiny"><?php echo $sp['version']; ?></a><?php } ?>
-					<?php if($sp['category'] != ''){ ?><a href="/version/<?php echo $sp['category']; ?>" class="button tiny"><?php echo $sp['category']; ?></a><?php } ?>
+					<?php if($sp['version'] != ''){ ?><a href="/version/<?php echo $sp['version']; ?>" class="button tiny"><?php echo $sp['version']; ?></a><?php } ?>
+					<?php if($sp['category'] != ''){ ?><a href="/category/<?php echo $sp['category']; ?>" class="button tiny"><?php echo $sp['category']; ?></a><?php } ?>
 				</div>
 			</div>
 <?php }
