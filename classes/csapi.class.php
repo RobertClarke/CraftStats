fa82d120dcc8e9a622cb1b77056abac3bde4c5bb
Alexander Miller <alexanderm781@gmail.com>
2013-06-05 18:07:38 -0700

    less hax

--- classes/csapi.class.php
+++ classes/csapi.class.php
@@ -592,15 +592,23 @@ class csAPI{
 		}
 	}
 	
+	function isDomain($domain_name){
+    
+	return (preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain_name) //valid chars check
+            && preg_match("/^.{1,253}$/", $domain_name) //overall length check
+            && preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain_name)   ); //length of each label
+	}
+	
 	private function getAddrByHost($host, $timeout = 1) {
-		$query = `nslookup -timeout=$timeout -retry=1 $host`;
-		if(preg_match('/\nAddress: (.*)\n/', $query, $matches))
-			return trim($matches[1]);
-			
-		$query = `nslookup -type=SRV -timeout=$timeout -retry=1 _minecraft._tcp.$host`;
-		if(preg_match('/service = (\d+) (\d+) (\d+) (.*)./', $query, $matches))
-			return trim($matches[4].':'.$matches[3]);
-		
+		if($this->isDomain($host)){
+			$query = `nslookup -timeout=$timeout -retry=1 $host`;
+			if(preg_match('/\nAddress: (.*)\n/', $query, $matches))
+				return trim($matches[1]);
+				
+			$query = `nslookup -type=SRV -timeout=$timeout -retry=1 _minecraft._tcp.$host`;
+			if(preg_match('/service = (\d+) (\d+) (\d+) (.*)./', $query, $matches))
+				return trim($matches[4].':'.$matches[3]);
+		}
 		return false;
 	}
 	
