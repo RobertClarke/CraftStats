8f2b935c23998e11c3e45beab1ad889082eb0016
RobertClarke <robert@rjfc.net>
2013-07-07 01:31:00 -0700

    Footer cleanup

--- templates/footer.php
+++ templates/footer.php
@@ -11,62 +11,11 @@
       </div>
     </div>
   </div>
-  
-  
-
-
-  
     <script>
     $(window).load(function(){
 		$("#featured").orbit();
 		
     });
-	/*jQuery(document).ready(function($) {
-		$(window).scroll(function () { 
-			if ($(this).scrollTop() > 75) {
-				$('.sidebar').addClass('sidebar-stick');
-			} else {
-				$('.sidebar').removeClass('sidebar-stick');
-			}
-		});
-	});*/
     </script> 
-
-
-  
-</body>
-<!-- this has absolutely nothing to do with robertbyte-crate-host (see lolrobert.com) or simplenode. Just to prove my point, here's a velociraptor:
-
-                                       O_
-                                      /  >
-                                      -  >   ^\
-                                     /   >  ^ /   
-                                    (O)  > ^ /   / / /  
-       _____                        |       |    \\|//
-      /  __ \                      _/      /     / _/
-     /  /  | |                    /       /     / /
-   _/  |___/ /                  _/      ------_/ / 
- ==_|  \____/                 _/       /  ______/
-     \   \                 __/           |\
-      |   \_          ____/              / \      _                    
-       \    \________/                  |\  \----/_V
-        \_         rawr craftstats.com  / \_______ V
-          \__                /       \ /          V
-             \               \        \
-              \______         \_       \
-                     \__________\_      \ 
-                        /    /    \_    | 
-                       |   _/       \   |
-                      /  _/          \  |
-                     |  /            |  |
-                     \  \__          |   \__
-                     /\____=\       /\_____=\ -->
-<!--
-            _               _                         _                   
-           | |             | |                       | |                  
-  _ __ ___ | |__   ___ _ __| |_  __      ____ _ ___  | |__   ___ _ __ ___ 
- | '__/ _ \| '_ \ / _ \ '__| __| \ \ /\ / / _` / __| | '_ \ / _ \ '__/ _ \
- | | | (_) | |_) |  __/ |  | |_   \ V  V / (_| \__ \ | | | |  __/ | |  __/
- |_|  \___/|_.__/ \___|_|   \__|   \_/\_/ \__,_|___/ |_| |_|\___|_|  \___|
--->
+  </body>
 </html>
