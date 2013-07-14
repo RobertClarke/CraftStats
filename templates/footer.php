3173957dfed2303497495e5e048936b539dd01d5
RobertClarke <robert@rjfc.net>
2013-07-13 23:00:24 -0700

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -4,10 +4,10 @@
     <div class="row">
       <div class="eight columns">
 		<div style="position:absolute;top:-28px;left:16px;color:#aaa;font-size:10px;"><?php include 'build.php'; ?></div>
-        lovingly made in <a href="http://twitter.com/redream_" target="_new">new zealand</a>, the <a href="http://twitter.com/robertjfclarke" target="_new">united states</a> and <a href="http://twitter.com/chrisoneillwood" target="_new">canada</a> 
+        a project by<a href="http://robertclarke.com" target="_new">Robert Clarke</a>
       </div>
       <div class="four columns" style="text-align:right;">
-        &lt;3's to <a href="http://minecraftservers.com/hosting/" target="_new">mcservers</a> and <a href="http://servercrate.com/minecraft" target="_new">servercrate</a>
+        &lt;3's to <a href="http://mcpestats.com" target="_new">mcpestats</a> and <a href="http://servercrate.com/minecraft" target="_new">servercrate</a>
       </div>
     </div>
   </div>
