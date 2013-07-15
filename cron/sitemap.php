ba8bf420ad3a4998d706457e723d5416eb3e7c76
Alexander Miller <alexanderm781@gmail.com>
2013-07-14 17:22:31 -0700

    xml

--- cron/sitemap.php
+++ cron/sitemap.php
@@ -4,59 +4,59 @@ $xml  = '';
 $xml .='<?xml version="1.0" encoding="UTF-8"?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<urlset>
+<url>
       <loc>http://www.craftstats.com/</loc>
-   </urlset>
-   <urlset>
+   </url>
+   <url>
       <loc>http://www.craftstats.com/hosting</loc>
-   </urlset>
-     <urlset>
+   </url>
+     <url>
       <loc>http://www.craftstats.com/submit</loc>
-   </urlset>
-   <urlset>
+   </url>
+   <url>
       <loc>http://www.craftstats.com/players</loc>
-   </urlset>
-   <urlset>
+   </url>
+   <url>
       <loc>http://www.craftstats.com/promote</loc>
-   </urlset>
-    <urlset>
+   </url>
+    <url>
       <loc>http://www.craftstats.com/category/new</loc>
-   </urlset>
-    <urlset>
+   </url>
+    <url>
       <loc>http://www.craftstats.com/category/active</loc>
-   </urlset>
-    <urlset>
+   </url>
+    <url>
       <loc>http://www.craftstats.com/category/reliable</loc>
-   </urlset>
+   </url>
 ';
 
 $servers = $database->query("SELECT ip, lastUpdate FROM servers WHERE uptime > -86400");
 foreach($servers as $s){
-	$xml .= '<urlset>
+	$xml .= '<url>
       <loc>http://www.craftstats.com/server/'.$s['ip'].'</loc>
 	  <lastmod>'.date(DATE_ATOM,$s['lastUpdate']).'</lastmod>
-   </urlset>';
+   </url>';
 }
 
 $host = $database->query("SELECT slug FROM hosts");
 foreach($host as $h){
-	$xml .= '<urlset>
+	$xml .= '<url>
       <loc>http://www.craftstats.com/host/'.$h['slug'].'</loc>
-   </urlset>';
+   </url>';
 }
 
  $vs = array_reverse($database->query("SELECT category FROM servers WHERE category != '' GROUP BY category")); 
   foreach($vs as $vb){
-	$xml .= '<urlset>
+	$xml .= '<url>
       <loc>http://www.craftstats.com/category/'.urlencode($vb['category']).'</loc>
-   </urlset>';
+   </url>';
   }
   
    $vs = array_reverse($database->query("SELECT version FROM versions ORDER BY time DESC, percent DESC LIMIT 5")); 
   foreach($vs as $vb){
-	$xml .= '<urlset>
+	$xml .= '<url>
       <loc>http://www.craftstats.com/version/'.urlencode($vb['version']).'</loc>
-   </urlset>';
+   </url>';
   }
 
 $xml .= '
