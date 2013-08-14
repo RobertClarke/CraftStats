39ec56095c26c27971d1d6914f1834ee8d7ddd47
RobertClarke <robert@rjfc.net>
2013-08-14 03:37:55 -0700

    Update promote.php

--- public_html/promote.php
+++ public_html/promote.php
@@ -64,13 +64,12 @@ if($_GET['paypal'] == 'paid' || $_GET['ttoken']){
 			}else{
 				$sv = $database->query("SELECT * FROM servers WHERE game = 'minecraft' AND ID = '$order[serverID]'",db::GET_ROW);
 				$responses = array(
-				'Check out this server http://minecraftservers.com/server/'.$sv['ip'],
-				'Congrats to these guys for becoming sponsored at MinecraftServers.com http://minecraftservers.com/server/'.$sv['ip'],
-				'Looking for a new Minecraft server to play on Look no further http://minecraftservers.com/server/'.$sv['ip'],
-				'Awesome new promoted server be sure to check these guys out http://minecraftservers.com/server/'.$sv['ip'],
+				'Check out this awesome Minecraft server! http://minecraftservers.com/server/'.$sv['ip'],
+				'Congrats to these guys for becoming sponsored at MinecraftServers.com! http://minecraftservers.com/server/'.$sv['ip'],
+				'Looking for a new Minecraft server to play on? Look no further! http://minecraftservers.com/server/'.$sv['ip'],
+				'Awesome new promoted server be sure to check these guys out! http://minecraftservers.com/server/'.$sv['ip'],
 				'We\'ve got another fantastic Minecraft server for you to try out http://minecraftservers.com/server/'.$sv['ip'],
-				'This minecraft server looks pretty awesome http://minecraftservers.com/server/'.$sv['ip'],
-				'This server looks pretty awesome http://minecraftservers.com/server/'.$sv['ip'],
+				'This minecraft server looks pretty awesome: http://minecraftservers.com/server/'.$sv['ip'],
 				'This is a great minecraft server http://minecraftservers.com/server/'.$sv['ip']);
 			}
 			$tmhOAuth = new tmhOAuth(array(
@@ -102,6 +101,7 @@ $template->setHeadScripts('<script>
 	   </script> ');
 $template->setKeys('minecraft advertising, more minecraft players ');
 $template->setTitle('Promote a Minecraft Server');
++$template->setDesc('Looking to get more players on your Minecraft server? Minecraft servers has a variety of different ways to promote your minecraft server.');
 $template->show('header');
 $template->show('nav');
 ?>
