328423e53c58ce9606f5f09ddda5f1e4c0d5231f
RobertClarke <robert@rjfc.net>
2013-08-14 17:00:10 -0700

    Update footer.php

--- public_html/dirtblock.com/inc/php/footer.php
+++ public_html/dirtblock.com/inc/php/footer.php
@@ -2,6 +2,6 @@
 	<div id="powered">
 		<span>Dirtblock.com is the proof of concept for the plugin "dirtblock"</span>
 		</br>
-		<span>Dirtblock the plugin will be used to track more server data on <a href="http://craftstats.com">craftstats.com</a></span>
+		<span>Dirtblock the plugin will be used to track more server data on <a href="http://minecraftservers.com">minecraftservers.com</a></span>
 	</div>
 </div>
