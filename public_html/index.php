1e06baf585505d0bb3ff8f848d4eb6a9a2936253
RobertClarke <robert@rjfc.net>
2013-08-17 13:32:45 -0700

    test 1

--- public_html/index.php
+++ public_html/index.php
@@ -15,9 +15,11 @@ if(strtolower($_GET['cat']) == 'active'){
 $template->setTitle(($_GET['version'] ? $_GET['version'].' Minecraft Servers' : ($_GET['cat'] ? $_GET['cat'].' Minecraft Servers':'Best Minecraft Servers List')));
 if($_GET['version']){
 	$template->setdesc('A list of the best Minecraft '.$_GET['version'].' servers for you to play on with your friends. These include '.$_GET['version'].' PVP Minecraft servers.');
+	$template->setKeys(($scat != '' ? 'minecraft '.$scat.' server, ':'').($scat != '' ? 'mc '.$scat.' server, ':'').' minecraft '.($server['version'] != '' ? $server['version'].' ' : '').'servers, '.($scat != '' ? 'minecraft '.$server['version'].' '.$scat.' servers, ':'').' '.($scat != '' ? 'minecraft '.$server['version'].' '.$scat.' server ':''));
 }
 if($_GET['cat']){
 	$template->setdesc('A list of the best Minecraft '.$_GET['cat'].' servers for you to play on with your friends. These include 1.6.2 '.$_GET['cat'].' Minecraft servers.');
+	$template->setKeys(($scat != '' ? 'minecraft '.$scat.' server, ':'').($scat != '' ? 'mc '.$scat.' server, ':'').' minecraft '.($server['version'] != '' ? $server['version'].' ' : '').'servers, '.($scat != '' ? 'minecraft '.$server['version'].' '.$scat.' servers, ':'').' '.($scat != '' ? 'minecraft '.$server['version'].' '.$scat.' server ':''));
 }
 
 $template->show('header');
