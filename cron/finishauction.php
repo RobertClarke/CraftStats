d3c6be0a17fa5b8f3e5f815ebbc3bba3cf14c7e3
RobertClarke <robert@rjfc.net>
2013-08-21 19:54:20 -0700

    Update craftstats -> minecraftservers

--- cron/finishauction.php
+++ cron/finishauction.php
@@ -11,11 +11,11 @@ $user = $database->query("SELECT * FROM users WHERE id = '$b[userID]'",db::GET_R
 $database->query("UPDATE promo_bids SET won = 1 WHERE id = '$b[id]'");
 $to      = $user['email'];
 $subject = 'CraftStats.com - You\'ve won this month\'s auction.';
-$message = '<img src="http://craftstats.com/images/logo.png"/><br/><br/>
+$message = '<img src="http://minecraftservers.com/images/logo.png"/><br/><br/>
 <b>Congratulations on winning the auction!<b><br/>
 Please pay for your bid within 72 hours, or you may be restricted from participating in future auctions. Also, make sure you set a banner on your server page!
-<br/><br/> <a href="http://craftstats.com/bid?pay='.$b['id'].'">Click here to activate your banner.</a>';
-$headers = 'From: noreply@craftstats.com'."\r\n";
+<br/><br/> <a href="http://minecraftservers.com/bid?pay='.$b['id'].'">Click here to activate your banner.</a>';
+$headers = 'From: noreply@minecraftserver.com'."\r\n";
 $headers  .= 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 mail($to,$subject,$message,$headers);
