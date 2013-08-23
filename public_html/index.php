04f5a99d039f27d885abbba55ec7c925ef8d7fc2
RobertClarke <robert@rjfc.net>
2013-08-23 16:57:44 -0700

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -51,9 +51,9 @@ if(strtolower($_GET['cat']) == 'vanilla'){
 if(strtolower($_GET['cat']) == 'hub'){
 	$_GET['cat'] = 'Hub';
 }
-$template->setTitle(($_GET['version'] ? $_GET['version'].' Minecraft Servers' : ($_GET['cat'] ? $_GET['cat'].' Minecraft Servers':'Minecraft Server List')));
+$template->setTitle(($_GET['version'] ? $_GET['version'].' Minecraft Servers' : ($_GET['cat'] ? $_GET['cat'].' Minecraft Servers':'')));
 
-$template->setKeys(($_GET['version'] ? $_GET['version'].' Minecraft Servers' : ($_GET['cat'] ? $_GET['cat'].' Minecraft Servers':'Minecaft Server List')));
+$template->setKeys(($_GET['version'] ? $_GET['version'].' Minecraft Servers' : ($_GET['cat'] ? $_GET['cat'].' Minecraft Servers':'')));
 
 if($_GET['version']){
 	$template->setdesc('A list of the best Minecraft '.$_GET['version'].' servers for you to play on with your friends. These include '.$_GET['version'].' PVP Minecraft servers.');
