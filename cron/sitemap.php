ec1c9f89bd37b3ecfef0d60a0f8e9d3ea38a7747
RobertClarke <robert@rjfc.net>
2013-09-05 17:52:12 -0700

    Update sitemap.php

--- cron/sitemap.php
+++ cron/sitemap.php
@@ -5,39 +5,39 @@ $xml .='<?xml version="1.0" encoding="UTF-8"?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url>
-      <loc>http://minecraftservers.com/</loc>
+      <loc>http://craftstats.com/</loc>
    </url>
    <url>
-      <loc>http://minecraftservers.com/hosting</loc>
+      <loc>http://craftstats.com/hosting</loc>
    </url>
      <url>
-      <loc>http://minecraftservers.com/submit</loc>
+      <loc>http://craftstats.com/submit</loc>
    </url>
    <url>
-      <loc>http://minecraftservers.com/players</loc>
+      <loc>http://craftstats.com/players</loc>
    </url>
    <url>
-      <loc>http://minecraftservers.com/promote</loc>
+      <loc>http://craftstats.com/promote</loc>
    </url>
     <url>
-      <loc>http://minecraftservers.com/category/new</loc>
+      <loc>http://craftstats.com/category/new</loc>
    </url>
     <url>
-      <loc>http://minecraftservers.com/category/active</loc>
+      <loc>http://craftstats.com/category/active</loc>
    </url>
     <url>
-      <loc>http://minecraftservers.com/category/reliable</loc>
+      <loc>http://craftstats.com/category/reliable</loc>
    </url>
 ';
 
 $servers = $database->query("SELECT ip, lastUpdate FROM servers WHERE uptime > -86400");
 foreach($servers as $s){
 	$xml .= '<url>
-      <loc>http://minecraftservers.com/server/'.$s['ip'].'</loc>
+      <loc>http://craftstats.com/server/'.$s['ip'].'</loc>
 	  <lastmod>'.date(DATE_ATOM,($s['lastUpdate'] == 0 ? time() : $s['lastUpdate'])).'</lastmod>
    </url>';
    $xml .= '<url>
-      <loc>http://minecraftservers.com/server/'.$s['ip'].'/vote</loc>
+      <loc>http://craftstats.com/server/'.$s['ip'].'/vote</loc>
 	  <lastmod>'.date(DATE_ATOM,($s['lastUpdate'] == 0 ? time() : $s['lastUpdate'])).'</lastmod>
    </url>';
 }
@@ -45,21 +45,21 @@ foreach($servers as $s){
 $host = $database->query("SELECT slug FROM hosts");
 foreach($host as $h){
 	$xml .= '<url>
-      <loc>http://minecraftservers.com/host/'.$h['slug'].'</loc>
+      <loc>http://craftstats.com/host/'.$h['slug'].'</loc>
    </url>';
 }
 
  $vs = array_reverse($database->query("SELECT category FROM servers WHERE category != '' GROUP BY category")); 
   foreach($vs as $vb){
 	$xml .= '<url>
-      <loc>http://minecraftservers.com/category/'.urlencode($vb['category']).'</loc>
+      <loc>http://craftstats.com/category/'.urlencode($vb['category']).'</loc>
    </url>';
   }
   
    $vs = array_reverse($database->query("SELECT version FROM versions ORDER BY time DESC, percent DESC LIMIT 5")); 
   foreach($vs as $vb){
 	$xml .= '<url>
-      <loc>http://minecraftservers.com/version/'.urlencode($vb['version']).'</loc>
+      <loc>http://craftstats.com/version/'.urlencode($vb['version']).'</loc>
    </url>';
   }
 
