40910b5efa0b2db45e7878a1d3967ed46e2bd3cb
RobertClarke <robert@rjfc.net>
2013-09-12 23:39:45 -0700

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="six columns">
 		<div style="position:absolute;top:-28px;left:16px;color:#aaa;font-size:10px;"><?php include 'build.php'; ?></div>
-        promote your server with <a href="/promote" target="_new">us</a>
+        the best list of <a href="http://craftstats.com/">minecraft servers</a>
       </div>
       <div class="six columns" style="text-align:right;">
         <!--&lt;3's to <a href="http://mojang.com" target="_new">mojang</a>-->
