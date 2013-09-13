90eea97f8ead8c28fb282a044e316ea54bc13288
RobertClarke <robert@rjfc.net>
2013-09-12 23:31:38 -0700

    Update sitemap.php

--- cron/sitemap.php
+++ cron/sitemap.php
@@ -6,6 +6,8 @@ $xml .='<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url>
       <loc>http://craftstats.com/</loc>
+      <priority>1</priority>
+      <changefreq>daily</changefreq>
    </url>
    <url>
       <loc>http://craftstats.com/hosting</loc>
