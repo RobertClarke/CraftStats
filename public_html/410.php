bbab7e882bbf4dbb9c7df4f0f0bf4b0aa5520155
RobertClarke <robert@rjfc.net>
2013-10-08 22:54:57 -0700

    new 410 page

---
+++ public_html/410.php
@@ -0,0 +1,13 @@
+<?php
+include '../inc/global.inc.php'; 
+$template->setTitle('410');
+$template->show('header');
+$template->show('nav');
+?>
+
+<h1 style="position:absolute;left:40px;top:50px;font-size:190px;">410</h1>
+<h2 style="position:absolute;left:40px;top:310px;font-size:16px;">The page you're looking for has been removed.</h2>
+</div>
+<?
+$template->show('footer');
+?>
