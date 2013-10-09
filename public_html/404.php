b8a1e76b4135a5b888b55f803cfad7507cdd054c
RobertClarke <robert@rjfc.net>
2013-10-08 22:55:44 -0700

    404 changes

--- public_html/404.php
+++ public_html/404.php
@@ -6,7 +6,7 @@ $template->show('nav');
 ?>
 
 <h1 style="position:absolute;left:40px;top:50px;font-size:190px;">404</h1>
-<h2 style="position:absolute;left:40px;top:310px;font-size:16px;">Maybe you could try looking <a href="/" style="color:#3A87AD;">somewhere else</a>?</h2>
+<h2 style="position:absolute;left:40px;top:310px;font-size:16px;">The page you're looking for cannot be found.</h2>
 </div>
 <?
 $template->show('footer');
