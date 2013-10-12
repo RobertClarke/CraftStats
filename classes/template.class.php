5bf18c04ca2c432a06e5588519ae49139bd1b521
RobertClarke <robert@rjfc.net>
2013-10-12 04:21:42 -0700

    If no keys, define keys

--- classes/template.class.php
+++ classes/template.class.php
@@ -78,6 +78,9 @@
 		if($this->desc == ''){
 			$this->setDesc('');
 		}
+		if($this->keys == ''){
+			$this->setKeys('');
+		}
 		$namef = strtoupper($name);
 		$this->modulecount++;
       $database = $this->db;
